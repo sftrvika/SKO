f3fafc0bdcff862539c4b5243df04771966b4d4c
jaystka <jaystka@students.amikom.ac.id>
2024-05-27 12:15:38 +0700

    Ourstory

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OurstoryController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ProfileController;
 
 Route::group(['middleware' => 'guest'], function () {
     Route::get('/', [HomeController::class, 'index']);
     Route::get('/shop', [ShopController::class, 'shopView']);
+    Route::get('/ourstory', [OurstoryController::class, 'ourstoryView'])->name('ourstory');
     Route::get('/register', [AuthController::class, 'registerView'])->name('register');
     Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
     Route::get('/login', [AuthController::class, 'loginView'])->name('login');
